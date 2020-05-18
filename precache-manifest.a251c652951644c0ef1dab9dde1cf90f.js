9383fc8b0dba0643c6ed0f642e6baf7a8b6ca811
farouk877 <faroukmarhaba1@gmail.com>
2020-05-18 19:29:23 -0400

    Updates

---
+++ precache-manifest.a251c652951644c0ef1dab9dde1cf90f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "802a06257be02f00fe2fef684b76c3f3",
+    "url": "/weathersenderapp/index.html"
+  },
+  {
+    "revision": "ad60fe5fe72edca2cf29",
+    "url": "/weathersenderapp/static/css/2.dafc00e9.chunk.css"
+  },
+  {
+    "revision": "166ef1eea3336b3ab4b0",
+    "url": "/weathersenderapp/static/css/main.1462a38f.chunk.css"
+  },
+  {
+    "revision": "ad60fe5fe72edca2cf29",
+    "url": "/weathersenderapp/static/js/2.d34ab998.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/weathersenderapp/static/js/2.d34ab998.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "166ef1eea3336b3ab4b0",
+    "url": "/weathersenderapp/static/js/main.055a6706.chunk.js"
+  },
+  {
+    "revision": "607611c5f21adf69f69a",
+    "url": "/weathersenderapp/static/js/runtime-main.e7fe0816.js"
+  },
+  {
+    "revision": "f197597c444ee302d89875b8f487f4f0",
+    "url": "/weathersenderapp/static/media/weather.f197597c.svg"
+  }
+]);
